dd1144e1cde4c5e7b0a26a44d9de8935aa1221a1
zhenghaowill2018 <41121335+zhenghaowill2018@users.noreply.github.com>
2023-03-17 11:08:52 +0800

    修改配置

--- settings.py
+++ settings.py
@@ -1,9 +1,8 @@
 #chatGPT api存放路径
-CHATGPT_API_KEI="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"
+CHATGPT_API_KEI="chatgpt_key"
 #index.json存放路径
 DATA_PATH="../ReviewData"
 #数据库配置
-DATABASE_DES_72={"host": "192.168.66.72","port": 3306,"user": "will","password": "zhenghao","db": "metis_formal_dev","autocommit": True}
-DATABASE_DES_71={"host": "192.168.66.71","port": 3306,"user": "will","password": "zhenghao","db": "metis_formal_dev","autocommit": True}
+DATABASE_DES_72={"host": "IP","port": 3306,"user": "账号","password": "密码","db": "库名","autocommit": True}
 #GPT模型
 GPT_MODEL="gpt-3.5-turbo"
